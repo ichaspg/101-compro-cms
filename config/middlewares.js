dcd78f3b698df340f015a149d50bc72ecb711dbe
ichaspg <ichaspurnama12@gmail.com>
2024-09-11 20:58:18 +0700

    update cms

--- config/middlewares.js
+++ config/middlewares.js
@@ -7,7 +7,11 @@ module.exports = [
     config: {
       enabled: true,
       headers: "*",
-      origin: ["http://localhost:1337", "http://localhost:3000"],
+      origin: [
+        "http://localhost:1337",
+        "http://localhost:3000",
+        "http://localhost:5173",
+      ],
     },
   },
   "strapi::logger",
